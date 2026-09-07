3782e354664d078832ad6b4875db624e1a38acf8
RogerioMartins <62802744+Rogeriomatador@users.noreply.github.com>
2026-09-07 18:32:42 -0300

    test: cover remote desktop codec preference

---
+++ app/src/test/java/dev/pocketpc/core/research/PocketPcResearchProbeTest.kt
@@ -0,0 +1,76 @@
+package dev.pocketpc.core.research
+
+import android.media.MediaFormat
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class PocketPcResearchProbeTest {
+    @Test
+    fun prefersHardwareAv1OverOtherCodecs() {
+        val codecs =
+            listOf(
+                ResearchCodec(
+                    name = "hw-avc",
+                    mimeType =
+                        MediaFormat.MIMETYPE_VIDEO_AVC,
+                    hardwareAccelerated = true,
+                    vendor = true,
+                ),
+                ResearchCodec(
+                    name = "hw-hevc",
+                    mimeType =
+                        MediaFormat.MIMETYPE_VIDEO_HEVC,
+                    hardwareAccelerated = true,
+                    vendor = true,
+                ),
+                ResearchCodec(
+                    name = "hw-av1",
+                    mimeType =
+                        MediaFormat.MIMETYPE_VIDEO_AV1,
+                    hardwareAccelerated = true,
+                    vendor = true,
+                ),
+            )
+
+        assertEquals(
+            PreferredRemoteCodec.AV1,
+            choosePreferredRemoteCodec(codecs),
+        )
+    }
+
+    @Test
+    fun ignoresSoftwareOnlyCodecForRemotePreference() {
+        val codecs =
+            listOf(
+                ResearchCodec(
+                    name = "sw-av1",
+                    mimeType =
+                        MediaFormat.MIMETYPE_VIDEO_AV1,
+                    hardwareAccelerated = false,
+                    vendor = false,
+                ),
+                ResearchCodec(
+                    name = "hw-avc",
+                    mimeType =
+                        MediaFormat.MIMETYPE_VIDEO_AVC,
+                    hardwareAccelerated = true,
+                    vendor = true,
+                ),
+            )
+
+        assertEquals(
+            PreferredRemoteCodec.AVC,
+            choosePreferredRemoteCodec(codecs),
+        )
+    }
+
+    @Test
+    fun reportsNoneWithoutConfirmedHardwareEncoder() {
+        assertEquals(
+            PreferredRemoteCodec.NONE,
+            choosePreferredRemoteCodec(
+                emptyList()
+            ),
+        )
+    }
+}
