ad08b4d69e9d60205c37928bc66b8ada00949337
RogerioMartins <62802744+Rogeriomatador@users.noreply.github.com>
2026-09-07 11:53:49 -0300

    desktop: make immersive inset mask explicit

--- app/src/main/java/dev/pocketpc/core/MainActivity.kt
+++ app/src/main/java/dev/pocketpc/core/MainActivity.kt
@@ -95,8 +95,9 @@ class MainActivity : ComponentActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.insetsController?.apply {
                 hide(
-                    WindowInsets.Type.statusBars() or
+                    WindowInsets.Type.statusBars().or(
                         WindowInsets.Type.navigationBars()
+                    )
                 )
                 systemBarsBehavior =
                     WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
