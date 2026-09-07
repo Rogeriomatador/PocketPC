d4a1d941561659a6630b14251e2ad816cbd73ef8
RogerioMartins <62802744+Rogeriomatador@users.noreply.github.com>
2026-09-07 19:17:41 -0300

    test: cover advertised remote stream profile selection

--- app/src/test/java/dev/pocketpc/core/research/PocketPcResearchProbeTest.kt
+++ app/src/test/java/dev/pocketpc/core/research/PocketPcResearchProbeTest.kt
@@ -77,4 +77,82 @@ class PocketPcResearchProbeTest {
             ),
         )
     }
+
+    @Test
+    fun choosesHighestAdvertisedProfileForPreferredCodec() {
+        val codecs =
+            listOf(
+                ResearchCodec(
+                    name = "hw-avc",
+                    mimeType = POCKET_MIME_AVC,
+                    hardwareAccelerated = true,
+                    vendor = true,
+                    surfaceInput = true,
+                    supports4k30 = true,
+                ),
+                ResearchCodec(
+                    name = "hw-av1",
+                    mimeType = POCKET_MIME_AV1,
+                    hardwareAccelerated = true,
+                    vendor = true,
+                    surfaceInput = true,
+                    supports720p60 = true,
+                    supports1080p60 = true,
+                    supports1440p60 = true,
+                ),
+            )
+
+        assertEquals(
+            AdvertisedRemoteStreamProfile.QHD_1440P60,
+            chooseAdvertisedRemoteStreamProfile(codecs),
+        )
+    }
+
+    @Test
+    fun doesNotBorrowResolutionSupportFromLowerPriorityCodec() {
+        val codecs =
+            listOf(
+                ResearchCodec(
+                    name = "hw-avc",
+                    mimeType = POCKET_MIME_AVC,
+                    hardwareAccelerated = true,
+                    vendor = true,
+                    surfaceInput = true,
+                    supports4k30 = true,
+                ),
+                ResearchCodec(
+                    name = "hw-av1",
+                    mimeType = POCKET_MIME_AV1,
+                    hardwareAccelerated = true,
+                    vendor = true,
+                    surfaceInput = true,
+                    supports720p60 = true,
+                ),
+            )
+
+        assertEquals(
+            AdvertisedRemoteStreamProfile.HD_720P60,
+            chooseAdvertisedRemoteStreamProfile(codecs),
+        )
+    }
+
+    @Test
+    fun reportsNoAdvertisedProfileWithoutHardwareSurfaceEncoder() {
+        val codecs =
+            listOf(
+                ResearchCodec(
+                    name = "sw-av1",
+                    mimeType = POCKET_MIME_AV1,
+                    hardwareAccelerated = false,
+                    vendor = false,
+                    surfaceInput = true,
+                    supports1080p60 = true,
+                ),
+            )
+
+        assertEquals(
+            AdvertisedRemoteStreamProfile.NONE,
+            chooseAdvertisedRemoteStreamProfile(codecs),
+        )
+    }
 }
