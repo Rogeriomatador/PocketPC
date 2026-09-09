742d02c4a83f89b12140eb627e460fbcebd47cdf
RogerioMartins <62802744+Rogeriomatador@users.noreply.github.com>
2026-09-09 17:29:37 -0300

    cleanup: remove obsolete Compose key compatibility shim

--- app/src/main/java/androidx/compose/ui/input/key/NativeKeyEventCompat.kt
+++
@@ -1,7 +0,0 @@
-package androidx.compose.ui.input.key
-
-import android.view.KeyEvent as AndroidKeyEvent
-
-/** Android compatibility shim for Compose versions where KeyEvent is the native Android event. */
-val KeyEvent.nativeKeyEvent: AndroidKeyEvent
-    get() = this as AndroidKeyEvent
