79382b4068204d7c612d2ac5cd15a8a8b813f9a4
RogerioMartins <62802744+Rogeriomatador@users.noreply.github.com>
2026-09-08 23:34:29 -0300

    runtime: expose bounded PocketPC Wine event pump

--- third_party/wine/pocketpc-driver/pocketpcdrv.h
+++ third_party/wine/pocketpc-driver/pocketpcdrv.h
@@ -18,12 +18,15 @@
 #include "pocketpc_display_bridge.h"
 #include "pocketpc_wine_window_bridge.h"
 
+#define POCKETPC_MAX_EVENTS_PER_PUMP 64u
+
 extern struct pdb_connection pocketpc_connection;
 extern struct pdb_wine_window_bridge pocketpc_windows;
 extern pthread_mutex_t pocketpc_bridge_mutex;
 extern BOOL pocketpc_bridge_ready;
 
 BOOL POCKETPC_CreateWindow(HWND hwnd);
+BOOL POCKETPC_ProcessEvents(DWORD mask);
 void POCKETPC_DestroyWindow(HWND hwnd);
 BOOL POCKETPC_WindowPosChanging(
     HWND hwnd,
