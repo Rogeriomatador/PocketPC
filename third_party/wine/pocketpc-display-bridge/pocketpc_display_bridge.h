eae9ee7a159b796a8443e6ef96156ea259874333
RogerioMartins <62802744+Rogeriomatador@users.noreply.github.com>
2026-09-08 23:35:55 -0300

    runtime: define display bridge v3 surface request contract

--- third_party/wine/pocketpc-display-bridge/pocketpc_display_bridge.h
+++ third_party/wine/pocketpc-display-bridge/pocketpc_display_bridge.h
@@ -6,7 +6,7 @@
 extern "C" {
 #endif
 #define PDB_MAGIC 0x31424450u
-#define PDB_VERSION 2u
+#define PDB_VERSION 3u
 #define PDB_HEADER_BYTES 20u
 #define PDB_MAX_PAYLOAD_BYTES 1048576u
 #define PDB_TOKEN_BYTES 32u
@@ -15,6 +15,7 @@ extern "C" {
 #define PDB_WINDOW_CREATE_BYTES 28u
 #define PDB_WINDOW_GEOMETRY_BYTES 40u
 #define PDB_WINDOW_DESTROY_BYTES 8u
+#define PDB_SURFACE_REQUEST_BYTES 32u
 #define PDB_SURFACE_AVAILABLE_BYTES 56u
 #define PDB_FRAME_READY_BYTES 32u
 #define PDB_POINTER_EVENT_BYTES 32u
@@ -25,6 +26,7 @@ extern "C" {
 #define PDB_MSG_WINDOW_CREATE 10u
 #define PDB_MSG_WINDOW_GEOMETRY 11u
 #define PDB_MSG_WINDOW_DESTROY 12u
+#define PDB_MSG_SURFACE_REQUEST 19u
 #define PDB_MSG_SURFACE_AVAILABLE 20u
 #define PDB_MSG_FRAME_READY 21u
 #define PDB_MSG_POINTER_EVENT 30u
@@ -60,6 +62,14 @@ extern "C" {
 #define PDB_ZORDER_ALLOWED (PDB_ZORDER_NO_CHANGE|PDB_ZORDER_TOP|PDB_ZORDER_BOTTOM|PDB_ZORDER_TOPMOST|PDB_ZORDER_NOTOPMOST|PDB_ZORDER_AFTER_WINDOW)
 struct pdb_connection { int fd; uint32_t negotiated_capabilities; uint64_t next_sequence; uint64_t expected_inbound_sequence; };
 struct pdb_frame { uint16_t type; uint64_t sequence; uint32_t payload_bytes; unsigned char *payload; };
+struct pdb_surface_request {
+    uint64_t window_id;
+    uint64_t generation;
+    int32_t width;
+    int32_t height;
+    uint32_t pixel_format;
+    uint32_t flags;
+};
 struct pdb_surface_available {
     uint64_t window_id;
     uint64_t surface_id;
@@ -93,6 +103,7 @@ int pdb_send_frame(struct pdb_connection*,uint16_t,const void*,uint32_t,char*,si
 int pdb_receive_frame(struct pdb_connection*,struct pdb_frame*,char*,size_t);
 int pdb_connection_has_input(struct pdb_connection*,char*,size_t);
 int pdb_receive_host_event(struct pdb_connection*,struct pdb_host_event*,char*,size_t);
+int pdb_send_surface_request(struct pdb_connection*,const struct pdb_surface_request*,char*,size_t);
 int pdb_receive_surface_available(struct pdb_connection*,struct pdb_surface_available*,char*,size_t);
 int pdb_surface_guest_path(const struct pdb_surface_available*,char*,size_t);
 int pdb_send_frame_ready(struct pdb_connection*,const struct pdb_frame_ready*,char*,size_t);
