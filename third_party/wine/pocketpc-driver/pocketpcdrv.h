2a5c4f6cf68aa4f885c0ea200e9d9fd1f784efb2
RogerioMartins <62802744+Rogeriomatador@users.noreply.github.com>
2026-09-08 23:15:19 -0300

    runtime: add Wine PocketPC driver template component

---
+++ third_party/wine/pocketpc-driver/pocketpcdrv.h
@@ -0,0 +1,43 @@
+#ifndef __WINE_POCKETPCDRV_H
+#define __WINE_POCKETPCDRV_H
+
+#ifndef __WINE_CONFIG_H
+#error You must include config.h before pocketpcdrv.h
+#endif
+
+#include <pthread.h>
+#include <stdint.h>
+
+#include "windef.h"
+#include "winbase.h"
+#include "winuser.h"
+#include "ntuser.h"
+#include "wine/gdi_driver.h"
+#include "wine/debug.h"
+
+#include "pocketpc_display_bridge.h"
+#include "pocketpc_wine_window_bridge.h"
+
+extern struct pdb_connection pocketpc_connection;
+extern struct pdb_wine_window_bridge pocketpc_windows;
+extern pthread_mutex_t pocketpc_bridge_mutex;
+extern BOOL pocketpc_bridge_ready;
+
+BOOL POCKETPC_CreateWindow(HWND hwnd);
+void POCKETPC_DestroyWindow(HWND hwnd);
+BOOL POCKETPC_WindowPosChanging(
+    HWND hwnd,
+    UINT swp_flags,
+    BOOL shaped,
+    const struct window_rects *rects
+);
+void POCKETPC_WindowPosChanged(
+    HWND hwnd,
+    HWND insert_after,
+    HWND owner_hint,
+    UINT swp_flags,
+    const struct window_rects *new_rects,
+    struct window_surface *surface
+);
+
+#endif
